006512385e1e133221e1e0c3e6e16e4df803b1ae
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-09-16 07:20:07 -0700

    created tables, seeded db

---
+++ ArtShare/app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true 
+
+    has_many :artworks,
+    foreign_key: :artist_id,
+    class_name: :Artwork 
+
+    has_many :artwork_views,
+    foreign_key: :viewer_id,
+    class_name: :ArtworkShare
+
+    has_many :shared_artworks,
+    through: :artwork_views,
+    source: :artwork 
+
+end 
--- ArtShare/db/seeds.rb
+++ ArtShare/db/seeds.rb
@@ -1,7 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Users
+u1 = User.create!(username: 'tristan')
+u2 = User.create!(username: 'snigdha')
+u3 = User.create!(username: 'michael')
+
+#Artworks
+a1 = Artwork.create!(title: 'panda', image_url: '3', artist_id: u1.id)
+a2 = Artwork.create!(title: 'plant', image_url: 'street', artist_id: u2.id)
+a3 = Artwork.create!(title: 'bridge', image_url: 'water', artist_id: u2.id)
+
+#ArtworkShares 
+as1 = ArtworkShare.create!(artwork_id: a1.id, viewer_id: u1.id)
+as2 = ArtworkShare.create!(artwork_id: a2.id, viewer_id: u1.id)
+as3 = ArtworkShare.create!(artwork_id: a3.id, viewer_id: u3.id)
